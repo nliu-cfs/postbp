313dd02856be40d5dfbc6cd4ab6bddf10843f5ae
Liu <ning.liu@nrcan-rncan.gc.ca>
2024-02-09 21:18:23 -0500

    Bump version: 0.0.2 → 0.0.3

--- postbp/__init__.py
+++ postbp/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ning Liu"""
 __email__ = "ning.liu@nrcan-rncan.gc.ca"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from postbp.common import prj2hex, pij_to_shp
 from postbp.otherFunctions import generate_ign_prob, generate_ESA, generate_burn_prob, generate_SSR, generate_fireshed
