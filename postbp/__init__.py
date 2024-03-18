9377462c61ae5cb223661f847e4491473fdaef55
nliu-cfs <ning.liu@nrcan-rncan.gc.ca>
2024-03-18 11:52:29 -0400

    removed . from import in init

--- postbp/__init__.py
+++ postbp/__init__.py
@@ -4,22 +4,22 @@ __author__ = """Ning Liu"""
 __email__ = "ning.liu@nrcan-rncan.gc.ca"
 __version__ = "0.0.3"
 
-from .common import (
+from common import (
     prj2hex,  #noqa
     pij_to_shp, #noqa
 )
-from .otherFunctions import (
+from otherFunctions import (
     generate_ign_prob,  #noqa
     generate_ESA,  #noqa
     generate_burn_prob,  #noqa
     generate_SSR,  #noqa
     generate_fireshed,   #noqa
 )
-from .spreadRose import (
+from spreadRose import (
     generate_fire_rose,  #noqa
     plot_rose,   #noqa
 )
-from .tessellation import (
+from tessellation import (
     create_hexagons_nodes,  #noqa
     create_arcs,  #noqa
     nodes_from_hexagons,  #noqa
